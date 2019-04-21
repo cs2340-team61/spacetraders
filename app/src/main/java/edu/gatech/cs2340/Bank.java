33ce72599973e08662c92b92c8e323d9a3eece84
Neil Patel <neilpatel10@yahoo.com>
2019-04-20 23:27:02 -0400

    bank
    added

---
+++ app/src/main/java/edu/gatech/cs2340/Bank.java
@@ -0,0 +1,19 @@
+package edu.gatech.cs2340;
+
+public class Bank {
+    private int bankCredits = 0;
+    private double interest = .05;
+
+    void deposit(int amount) {
+        bankCredits += amount;
+    }
+
+    void withdraw(int amount) {
+        bankCredits -= amount;
+    }
+
+    void addInterest() {
+        bankCredits += bankCredits * interest;
+    }
+
+}
